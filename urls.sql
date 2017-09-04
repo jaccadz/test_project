9130b572365e041ffbc328562870167bea987208
Jacob Cadelina <jaccadz1@gmail.com>
2017-09-04 17:14:02 +0800

    fixed config.yml

--- urls.sql
+++ urls.sql
@@ -96,6 +96,8 @@ UNLOCK TABLES;
 DROP TABLE IF EXISTS `users`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
+
+
 CREATE TABLE `users` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
